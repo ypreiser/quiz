9797628df1bd0404df4b4c650cd5922e966d8f34
eliraz <elish7@gmail.com>
2024-06-05 11:12:55 +0300

    after pull

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: '*', methods: '*' } });
+
+const players = []; // רשימה לאחסון כל השחקנים
+
+io.on('connection', (socket) => {
+    // console.log('a user connected:', socket.id);
+
+    socket.on('joinGame', (userData) => {
+        const newPlayer = { id: socket.id, ...userData };
+        // console.log('new player joined:', newPlayer);
+        socket.join(newPlayer.id);
+        players.push(newPlayer);
+        // console.log('players:', players);
+        io.emit('joinGame', players);
+    });
+
+    socket.on('updatePlayerList', (playerList) => {
+        // console.log('updatePlayerList', playerList);
+     
+        io.emit('updatePlayerList', playerList);
+    });
+
+    socket.on('disconnect', () => {
+        // console.log('user disconnected:', socket.id);
+        const index = players.findIndex(player => player.id === socket.id);
+        if (index !== -1) {
+            players.splice(index, 1);
+            io.emit('updatePlayerList', players);
+        }
+    });
+});
+
+server.listen(3000, () => console.log("Listening on port 3000"));
